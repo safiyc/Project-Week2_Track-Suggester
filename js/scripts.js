3ec782a18f62735b9938e60b4d222f4ce670e898
Safiy Cham <csafi13@yahoo.com>
2017-08-11 13:21:43 -0700

    add rows to webpage, add jquery hide/show result, add css colors/padding/margin

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
   $("#survey").submit(function(event) {
-    even.prevenDefault();
-  var answer1 = $("input:radio[name=answer1]:checked").val();
-  var answer2 = $("input:radio[name=answer2]:checked").val();
-  var answer3 = $("input:radio[name=answer3]:checked").val();
-  var answer4 = $("input:radio[name=answer4]:checked").val();
-  var answer4 = $("input:radio[name=answer5]:checked").val();
+    event.preventDefault();
+    $("#result_ruby, #result_java, #result_csharp").hide();
+    var answer1 = parseInt($("input:radio[name=answer1]:checked").val());
+    var answer2 = parseInt($("input:radio[name=answer2]:checked").val());
+    var answer3 = parseInt($("input:radio[name=answer3]:checked").val());
+    var answer4 = parseInt($("input:radio[name=answer4]:checked").val());
+    var answer5 = parseInt($("input:radio[name=answer5]:checked").val());
 
-  var result = answer1 + answer2 + answer3 + answer4 + answer5;
+    var result = answer1 + answer2 + answer3 + answer4 + answer5;
 
-    if (result <= 5) {
-      $("#")
+    if (result >= 8 && result <= 10) {
+      $("#result_java").show();
+    }
+    else if (result >= 11) {
+      $("#result_csharp").show();
+    }
+    else {
+      $("#result_ruby").show();
     }
   });
 });
